9bfa30d4cdacf8c957c496698c3d6005c9f7dc80
Abhishek Kumar Singh <idforabhilaptop@gmail.com>
2026-05-07 12:32:37 +0530

    11: Agents Form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,23 +1,38 @@
 
 import { ErrorState } from '@/components/error-state'
 import { LoadingState } from '@/components/loading-state'
+import { auth } from '@/lib/auth'
+import { AgentsListHeader } from '@/modules/agents/ui/components/agents-list-header'
 import { AgentView } from '@/modules/agents/ui/views/agent-view'
 import { getQueryClient, trpc } from '@/trpc/server'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import React, { Suspense } from 'react'
 import { ErrorBoundary } from "react-error-boundary"
 
 const page = async () => {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) {
+        redirect("/signin")
+    }
+
     const queryClient = getQueryClient()
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions())
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<LoadingState title="Loading Agents" description="This may take few seconds." />}>
-                <ErrorBoundary fallback = {<ErrorState title="Error Occurred" description="Failed to load agents." />}>
-                    <AgentView />
-                </ErrorBoundary>
-            </Suspense>
-        </HydrationBoundary>
+        <>
+            <AgentsListHeader />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<LoadingState title="Loading Agents" description="This may take few seconds." />}>
+                    <ErrorBoundary fallback={<ErrorState title="Error Occurred" description="Failed to load agents." />}>
+                        <AgentView />
+                    </ErrorBoundary>
+                </Suspense>
+            </HydrationBoundary>
+        </>
     )
 }
 
