323581068614bea3f74407998a17cb43749fa52b
Abhishek Kumar Singh <idforabhilaptop@gmail.com>
2026-05-26 11:03:25 +0530

    18: meeting data table

--- src/modules/meetings/ui/view/meetings-view.tsx
+++ src/modules/meetings/ui/view/meetings-view.tsx
@@ -1,8 +1,11 @@
 
 "use client"
 
+import { DataTable } from "@/components/data-table"
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
+import { columns } from "../components/columns"
+import { EmptyState } from "@/components/empty-state"
 
 const MeetingsView = () => {
     const trpc = useTRPC()
@@ -10,6 +13,11 @@ const MeetingsView = () => {
     return (
         <div>
             {/* {JSON.stringify(data)} */}
+            <DataTable data={data.items} columns={columns} />
+            {data.items.length === 0 && (
+                <EmptyState title="Create your first meeting"
+                    description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in real time." />
+            )}
         </div>
     )
 }
